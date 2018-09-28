d1075e04c1ae2bcdf4d8ad36dff3ad3147dc8be0
theprophetofnod <42500414+theprophetofnod@users.noreply.github.com>
2018-09-27 19:04:20 -0500

    Add files via upload

--- Ian/driveTrainOpMode.java
+++ Ian/driveTrainOpMode.java
@@ -18,63 +18,22 @@ public class driveTrainOpMode extends OpMode
     public void init(){
         robot.init(hardwareMap);
     }
+    
     @Override
-    public void init_loop(){
-    }
-    @Override
+	//to start the the robot
     public void start(){
     }
-    @Override
-    public void loop() {
-        double gamepad1SpeedND = gamepad1.left_stick_y;
-        double gamepad1T = gamepad1.left_stick_x;
-        double power = 0.06;
-        double rewop = -0.06;
-
-        //turning the robot!!
-        if(Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND) ||
-        Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND)){
-           //counter clock wise
-            if(gamepad1T > power){
-                robot.right1.setPower(gamepad1T);
-                robot.right2.setPower(gamepad1T);
-                robot.left1.setPower(-gamepad1T);
-                robot.left2.setPower(-gamepad1T);
-            }
-            // clock wise
-            else if(gamepad1T < rewop){
-                robot.right1.setPower(-gamepad1T);
-                robot.right2.setPower(-gamepad1T);
-                robot.left1.setPower(gamepad1T);
-                robot.left2.setPower(gamepad1T);
-            }
-            else{
-                robot.right1.setPower(0);
-                robot.right2.setPower(0);
-                robot.left1.setPower(0);
-                robot.left2.setPower(0);
-            }
-        }
-        //normal driving!
-        // right
-       else{
-            if (gamepad1SpeedND > power) {
-                robot.right1.setPower(gamepad1SpeedND);
-                robot.right2.setPower(gamepad1SpeedND);
-                robot.left1.setPower(gamepad1SpeedND);
-                robot.left2.setPower(gamepad1SpeedND);
-            } else if (gamepad1SpeedND < rewop) {
-                robot.right1.setPower(gamepad1SpeedND);
-                robot.right2.setPower(gamepad1SpeedND);
-                robot.left1.setPower(-gamepad1SpeedND);
-                robot.left2.setPower(-gamepad1SpeedND);
-            } else {
-                robot.right1.setPower(0);
-                robot.right2.setPower(0);
-                robot.left1.setPower(0);
-                robot.left2.setPower(0);
-            }
-        }
+   @Override
+   public void loop() {
+	 // it need the AI control of the robot for the driverTrain.
+	 robot.init();
+	   
+   }
+   @Override
+	// it adds the movement fuction to the start of the robot planes (driver control). 
+	public void loop() {
+		robot.init(accelerate);
+	
     }
+    
 }
-
