680e8e9d70dde5f0175e4cdb4aa52dbec23c7226
theprophetofnod <42500414+theprophetofnod@users.noreply.github.com>
2018-10-04 11:10:30 -0500

    Update driveTrainHardwareMapArrays.java

--- Ian/driveTrainHardwareMapArrays.java
+++ Ian/driveTrainHardwareMapArrays.java
@@ -1,9 +1,8 @@
 // arays versoin of the code.
 package org.firstinspires.ftc.teamcode;
-//working controls
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
+import Java.util.Array;
 /**
  * This is NOT an opmode.
  *
@@ -30,67 +29,63 @@ public class driveTrainHardwareMapArrays {
     /* Constructor */
 	double power  = 0.06;
 	double rewop  = -0.06;
-   double powerThreshold = 0.06;
+    double powerThreshold = 0.06;
+	
     /* Initialize standard Hardware interfaces */
-    public void init(HardwareMap ahwMap) {
+	public void init(HardwareMap ahwMap) {
         /* Save reference to Hardware map */
         hwMap = ahwMap;
 
-		double[] Motors
-	   Motors = new double[4]
-	   index = 0;
-	  while(index < 4) {
-	    Motors[0,1,2,3] = hwMap.get(DcMotor.class);
-		Motor[0,1,2,3,].setDirection(DcMotor.Direction.FORWARD0;
-		Motor[0,1,2,3].setPower(0);
-		index++;  
-		check index;
-	  
-	}  /* fuction for movement@! */
-	   public void movement (double gamepad1SpeedND,double gamepad1T) {
+    	double[] Motors
+	    Motors = new double[4]
+		double[] Nmotors
+		Nmotors = new double[3]
+		/*setting the verable of the  array */
+		for(double j = 0; j,Nmotors.length; j++){
+		 Nmotors[j] = hwMap.get(DcMotor.class);
+		 Nmotors[j].setDirection(DcMotor.Direction.FORWARD);
+		 Nmotors[j].setPower(0);
+		}
+		for(double i = 0; i < Motors.length; i++){
+		 Motors[i] = hwMap.get(DcMotor.class);
+		 Motors[i].setDirection(DcMotor.Direction.FORWARD;
+		 Motors[i].setPower(0);
+		}  
+	}
+	
+	/* fuction for movement@! */
+public void movement (double gamepad1SpeedND,double gamepad1T) {
 		
-		 if(Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND) ||
-        Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND)){
-           /* rotating counter clock wise */
-            if(gamepad1T > power){
-                Motor[0].setPower(gamepad1T);
-                Motor[1].setPower(gamepad1T);
-                Motor[2].setPower(-gamepad1T);
-                Motor[3].setPower(-gamepad1T);
-            }
-            /* rotating clock wise */
-            else if(gamepad1T < rewop){
-                Motor[0].setPower(-gamepad1T);
-                Motor[1].setPower(-gamepad1T);
-                Motor[2].setPower(gamepad1T);
-                Motor[3].setPower(gamepad1T);
-            }
-            else{
-                Motor[0].setPower(0);
-                Motor[1].setPower(0);
-                Motor[2].setPower(0);
-                Motor[3].setPower(0);
-            }
-        } 
-			else{
-				if(){
-                Motor[0].setPower(gamepad1SpeedND);
-                Motor[1].setPower(gamepad1SpeedND);
-                Motor[2].setPower(gamepad1SpeedND);
-                Motor[3].setPower(gamepad1SpeedND);
-				} 
-				else if (gamepad1SpeedND < rewop) {
-                Motor[0].setPower(-gamepad1SpeedND);
-                Motor[1].setPower(-gamepad1SpeedND);
-                Motor[2].setPower(-gamepad1SpeedND);
-                Motor[3].setPower(-gamepad1SpeedND);
+	
+		for(i,j = 0; i < Motors.length, j < Nmotors.length; j,i++){
+			
+			if(Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND) ||
+			Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND)){		
+				if(gamepa1T > power){
+					Motors[i].setPower(gamepad1T);
+					Nmotors[j].setpower(-gamepad1T);
 				}
-				else {
-                Motor[0].setPower(0);
-                Motor[1].setPower(0);
-                Motor[2].setPower(0);
-                Motor[3].setPower(0);
+				else if (gamepa1T < rewop){
+					Motors[i].setPower(-gamepad1T);
+					Nmotors[j].setPower(gamepad1T);
+					
+				}
+				else{
+				Motor[i].setPower(0);	
+				Nmotors[j].setPower(0);
+				}
+			
+			}
+			esle{
+				if(gamepad1SpeedND > power){
+					Motors[i].setPower(gamepad1SpeedND);
+				}
+				else if (gamepad1SpeedND < rewop){
+					Motors[i].setPower(-gamepad1SpeedND);
+				}
+				else{
+					Motors[i].setPower(0);
 				}
 			}
 		}
-}
+	}
