0101db687a1777a596ad1d73d9ccb4a648086fc0
theprophetofnod <42500414+theprophetofnod@users.noreply.github.com>
2018-10-04 20:29:45 -0500

    Add files via upload

--- Ian/driveTrainHardwareMapArrays.java
+++ Ian/driveTrainHardwareMapArrays.java
@@ -2,7 +2,7 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import Java.util.Array;
+/* right tank control */
 /**
  * This is NOT an opmode.
  *
@@ -21,83 +21,99 @@ import Java.util.Array;
  */
 public class driveTrainHardwareMapArrays {
     /* Public OpMode members. */
-	public  power;
-	public  rewop;
+    /*construcking the arrays variable*/
+    DcMotor[] motors;
 
     /* local OpMode members. */
     HardwareMap hwMap   =  null;
     /* Constructor */
-	double power  = 0.06;
-	double rewop  = -0.06;
+    double power  = 0.06;
+    double rewop  = -0.06;
     double powerThreshold = 0.06;
-	
+
     /* Initialize standard Hardware interfaces */
-	public void init(HardwareMap ahwMap) {
+    public void init(HardwareMap ahwMap) {
         /* Save reference to Hardware map */
         hwMap = ahwMap;
-		/*construcking the arrays variable*/
-    	double[] Motors
-	    Motors = new double[4]
-		double[] Nmotors
-		Fmotors = new double[3]
-		double[] Smotors
-		Bmotors = new double[3]
-		
-		/*give the arrays there infomation to work */
-		for(double m = 0; j,Nmotors.length; j++){
-		 Lmotors[m] = hwMap.get(DcMotor.class);
-		 Lmotors[m].setDirection(DcMotor.Direction.FORWARD);
-		 Lmotors[m].setPower(0);
-		}
-		for(double j = 0; j,Nmotors.length; j++){
-		 Rmotors[j] = hwMap.get(DcMotor.class);
-		 Rmotors[j].setDirection(DcMotor.Direction.FORWARD);
-		 Rmotors[j].setPower(0);
-		}
-		for(double i = 0; i < Motors.length; i++){
-		 Motors[i] = hwMap.get(DcMotor.class);
-		 Motors[i].setDirection(DcMotor.Direction.FORWARD;
-		 Motors[i].setPower(0);
-		}  
-	}
-	
-	/* fuction for movement@! */
-public void movementByControl (double gamepad1SpeedND,double gamepad1T) {
-		
-	
-		for(i,j,m = 0; m < Smotors.length, i < Motors.length, j < Nmotors.length; m,j,i++){
-			
-			if(Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND) ||
-			Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND)){		
-				if(gamepa1T > power){
-					Lmotors[m].setPower(gamepad1T);
-					Rmotors[j].setpower(-gamepad1T);
-				}
-				else if (gamepa1T < rewop){
-					Lmotors[m].setPower(-gamepad1T);
-					Rmotors[j].setPower(gamepad1T);
-					
-				}
-				else{
-				Lmotor[m].setPower(0);	
-				Rmotors[j].setPower(0);
-				}
-			
-			}
-			esle{
-				//saying if gamepad1SpeedND is greater than power that is 0.06 than move Motors[i] in a postive way.
-				if(gamepad1SpeedND > power){
-					Motors[i].setPower(gamepad1SpeedND);
-				}
-				//saying if gamepad1SpeedND is lessthan than rewop that is -0.06 than move Motors[i] in a negative way.
-				else if (gamepad1SpeedND < rewop){
-					Motors[i].setPower(-gamepad1SpeedND);
-				}
-				//saying if the Motor[i] is not doing anything than have the power of the motor to 0.
-				else{
-					Motors[i].setPower(0);
-				}
-			}
-		}
-	}
 
+
+        /*give the arrays there infomation to work */
+        for(int i = 0; i < motors.length; i++){
+            motors[i] = hwMap.get(DcMotor.class, "motor"+i);
+            motors[i].setDirection(DcMotor.Direction.FORWARD);
+            motors[i].setPower(0);
+            motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+
+    /* fuction for movement@! */
+    public void tankcontrolsMovent (double gamepad1Ry ,double gamepad1Ly){
+        if(gamepad1Ly > power || gamepad1Ly < power){
+            motors[0].setPower(gamepad1Ly);
+            motors[2].setPower(gamepad1Ly);
+        }
+        else{
+            motors[0].setPower(0);
+            motors[2].setPower(0);
+        }
+        if(gamepad1Ry > power || gamepad1Ry < power){
+            motors[1].setPower(-gamepad1Ry);
+            motors[3].setPower(-gamepad1Ry);
+
+        }
+        else{
+            motors[1].setPower(0);
+            motors[3].setPower(0);
+        }
+
+    }
+
+    public void movementByControl (double gamepad1SpeedND,double gamepad1T) {
+
+            if(Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND)){
+                if(gamepad1T > power){
+                    motors[0].setPower(gamepad1T);
+                    motors[1].setPower(-gamepad1T);
+                    motors[2].setPower(gamepad1T);
+                    motors[3].setPower(-gamepad1T);
+                }
+                else if (gamepad1T < rewop){
+                    motors[0].setPower(-gamepad1T);
+                    motors[1].setPower(gamepad1T);
+                    motors[2].setPower(gamepad1T);
+                    motors[3].setPower(-gamepad1T);
+                }
+                else{
+                    motors[0].setPower(0);
+                    motors[1].setPower(0);
+                    motors[2].setPower(0);
+                    motors[3].setPower(0);
+                }
+
+            }
+            else{
+                //saying if gamepad1SpeedND is greater than power that is 0.06 than move Motors[i] in a postive way.
+                if(gamepad1SpeedND > power){
+                    motors[0].setPower(gamepad1SpeedND);
+                    motors[1].setPower(gamepad1SpeedND);
+                    motors[2].setPower(gamepad1SpeedND);
+                    motors[3].setPower(gamepad1SpeedND);
+                }
+                //saying if gamepad1SpeedND is lessthan than rewop that is -0.06 than move Motors[i] in a negative way.
+                else if (gamepad1SpeedND < rewop){
+                    motors[0].setPower(-gamepad1SpeedND);
+                    motors[1].setPower(-gamepad1SpeedND);
+                    motors[2].setPower(-gamepad1SpeedND);
+                    motors[3].setPower(-gamepad1SpeedND);
+                }
+                //saying if the Motor[i] is not doing anything than have the power of the motor to 0.
+                else{
+                    motors[0].setPower(0);
+                    motors[1].setPower(0);
+                    motors[2].setPower(0);
+                    motors[3].setPower(0);
+
+                }
+            }
+        }
+    }
