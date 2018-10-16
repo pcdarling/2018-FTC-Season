282bd1fb9ce94408baca262746f0f8a03d2b8e62
theprophetofnod <42500414+theprophetofnod@users.noreply.github.com>
2018-10-15 20:11:04 -0500

    Update driveTrainOpMode.java

--- Ian/driveTrainOpMode.java
+++ Ian/driveTrainOpMode.java
@@ -13,18 +13,22 @@ public class driveTrainOpMode extends OpMode {
     public void init(){
         robot.init(hardwareMap);
     }
-    
+
     @Override
-	//to start the the robot
+    //to start the the robot
     public void start(){
+        double xA = 0.06;
+        double xB =14.13 ;
+        boolean xC = false;
+        robot.inchesmovement(xA,xB,xC);
 
     }
-   @Override
-	// it adds the movement fuction to the start of the robot planes (driver control). 
-	public void loop() {
-       double Rx = gamepad1.right_stick_x;
-       double Ry = gamepad1.right_stick_y;
-       double Ly = gamepad1.left_stick_y;
+    @Override
+    // it adds the movement fuction to the start of the robot planes (driver control).
+    public void loop() {
+        double Rx = gamepad1.right_stick_x;
+        double Ry = gamepad1.right_stick_y;
+        double Ly = gamepad1.left_stick_y;
         if(tankControls) {
             robot.tankcontrolsMovent(Ry,Ly);
 
@@ -32,7 +36,7 @@ public class driveTrainOpMode extends OpMode {
         else {
             robot.movementByControl(Rx,Ly);
         }
-	
+
     }
-    
+
 }
