9bf69cf6db10e556814861651e9ee8e9927b62d2
theprophetofnod <42500414+theprophetofnod@users.noreply.github.com>
2018-10-27 17:02:35 -0500

    Update driveTrainHardwareMapArrays.java

--- Ian/driveTrainHardwareMapArrays.java
+++ Ian/driveTrainHardwareMapArrays.java
@@ -1,10 +1,7 @@
 // arays versoin of the code.
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.hardware.lynx.commands.core.LynxGetMotorTargetPositionCommand;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import java.lang.annotation.Target;
 
 public class driveTrainHardwareMapArrays {
     /* Public OpMode members. */
@@ -14,8 +11,14 @@ public class driveTrainHardwareMapArrays {
     /* local OpMode members. */
     HardwareMap hwMap   =  null;
     /* Constructor */
-    double power  = 0.06;
-    double rewop  = -0.06;
+    double thresh  = 0.06;
+    double encCountsPerRev = 20 * 20 * 84 / 100; // electrical * internal * external
+    double wheelRadius = 2.25;
+    double wheelCircumference = 2 * Math.PI * wheelRadius;
+    double robotLength = 14.5;
+    double robotWidth = 17.5;
+    double robotDiameter = Math.sqrt(Math.pow(robotLength,2)+Math.pow(robotWidth,2));
+    double robotCircumference = Math.PI*robotDiameter;
     /* Initialize standard Hardware interfaces */
 
     public void init(HardwareMap ahwMap) {
@@ -29,88 +32,111 @@ public class driveTrainHardwareMapArrays {
             motors[i].setDirection(DcMotor.Direction.FORWARD);
             motors[i].setPower(0);
             motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         }
     }
-    public void inchesmovement (double power1,double inches ,boolean rotation) {
-        int enCountsPerRev =20;
-        double internalGear =40;
-        double ExternalGearRation = 80/100;
-        double trueCountaPerRev = (double)enCountsPerRev*internalGear*ExternalGearRation;
-        int start = 1;
-        
+
+    public void driveInInches (double power,double inches,boolean rotation) {
+        boolean busy = true;
+        double percentOfWheel = inches / wheelCircumference;
+        int counts = (int)(percentOfWheel * encCountsPerRev);
+        int sign = 1;
+
+        if (rotation) {
             for (int i = 0; i < motors.length; i++) {
-                motors[i].setTargetPosition(motors[0].getCurrentPosition() * (int) 1.910 * 20);
+                if (power > 0) {
+                    sign = -1;
+                } else {
+                    sign = 1;
+                }
+                int currentPosition = motors[i].getCurrentPosition();
                 motors[i].setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motors[i].setPower(power1);
-    }
-    public void tankcontrolsMovent (double gamepad1Ry ,double gamepad1Ly){
-        if(gamepad1Ly > power || gamepad1Ly < power){
-            motors[0].setPower(gamepad1Ly);
-            motors[2].setPower(gamepad1Ly);
-        }
-        else{
-            motors[0].setPower(0);
-            motors[2].setPower(0);
-        }
-        if(gamepad1Ry > power || gamepad1Ry < power){
-            motors[1].setPower(-gamepad1Ry);
-            motors[3].setPower(-gamepad1Ry);
-
-        }
-        else{
-            motors[1].setPower(0);
-            motors[3].setPower(0);
-        }
-
-    }
-
-    public void movementByControl (double gamepad1SpeedND,double gamepad1T) {
-
-            if(Math.abs(gamepad1T) > Math.abs(gamepad1SpeedND)){
-                if(gamepad1T > power){
-                    motors[0].setPower(-gamepad1T);
-                    motors[1].setPower(gamepad1T);
-                    motors[2].setPower(-gamepad1T);
-                    motors[3].setPower(gamepad1T);
-                }
-                else if (gamepad1T < rewop){
-                    motors[0].setPower(-gamepad1T);
-                    motors[1].setPower(gamepad1T);
-                    motors[2].setPower(-gamepad1T);
-                    motors[3].setPower(gamepad1T);
-
-                }
-                else{
-                    motors[0].setPower(0);
-                    motors[1].setPower(0);
-                    motors[2].setPower(0);
-                    motors[3].setPower(0);
-                }
-
+                motors[i].setTargetPosition(currentPosition + sign * counts);
+                motors[i].setPower(power);
             }
-            else{
-                //saying if gamepad1SpeedND is greater than power that is 0.06 than move Motors[i] in a postive way.
-                if(gamepad1SpeedND > power){
-                    motors[0].setPower(-gamepad1SpeedND);
-                    motors[1].setPower(-gamepad1SpeedND);
-                    motors[2].setPower(gamepad1SpeedND);
-                    motors[3].setPower(gamepad1SpeedND);
+        } else {
+            for (int i = 0; i < motors.length; i++) {
+                int even = i%2;
+                if(even == 0){
+                    if (power > 0) {
+                        sign = 1;
+                    } else {
+                        sign = -1;
+                    }
                 }
-                //saying if gamepad1SpeedND is lessthan than rewop that is -0.06 than move Motors[i] in a negative way.
-                else if (gamepad1SpeedND < rewop){
-                    motors[0].setPower(-gamepad1SpeedND);
-                    motors[1].setPower(-gamepad1SpeedND);
-                    motors[2].setPower(gamepad1SpeedND);
-                    motors[3].setPower(gamepad1SpeedND);
+                else {
+                    if (power > 0) {
+                        sign = -1;
+                    } else {
+                        sign = 1;
+                    }
                 }
-                //saying if the Motor[i] is not doing anything than have the power of the motor to 0.
-                else{
-                    motors[0].setPower(0);
-                    motors[1].setPower(0);
-                    motors[2].setPower(0);
-                    motors[3].setPower(0);
 
-                }
+                int currentPosition = motors[i].getCurrentPosition();
+                motors[i].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motors[i].setTargetPosition(currentPosition + sign * counts);
+                motors[i].setPower(power);
             }
         }
+
+        while (busy) {
+            for (int i = 0; i < motors.length; i++) {
+                busy = busy && motors[i].isBusy();
+            }
+        }
+
+        for (int i = 0; i < motors.length; i++) {
+            motors[i].setPower(0);
+            motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
     }
+
+    public void driveInInches (double power,double inches) {
+        boolean busy = true;
+        double percentOfWheel = inches / wheelCircumference;
+        int counts = (int)(percentOfWheel * encCountsPerRev);
+        int sign = 1;
+
+
+        for (int i = 0; i < motors.length; i++) {
+            int even = i%2;
+            if(even == 0){
+                if (power > 0) {
+                    sign = 1;
+                } else {
+                    sign = -1;
+                }
+            }
+            else {
+                if (power > 0) {
+                    sign = -1;
+                } else {
+                    sign = 1;
+                }
+            }
+
+            int currentPosition = motors[i].getCurrentPosition();
+            motors[i].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motors[i].setTargetPosition(currentPosition + sign * counts);
+            motors[i].setPower(power);
+        }
+
+
+        while (busy) {
+            for (int i = 0; i < motors.length; i++) {
+                busy = busy && motors[i].isBusy();
+            }
+        }
+
+        for (int i = 0; i < motors.length; i++) {
+            motors[i].setPower(0);
+            motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+    }
+
+    public void rotateInDegrees(double power,double degrees){
+        driveInInches(power,degrees/360*robotCircumference,true);
+    }
+}
