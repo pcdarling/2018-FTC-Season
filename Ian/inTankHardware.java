a4e776be9c3a9f59f89e80c9eed0bc92254fad79
theprophetofnod <42500414+theprophetofnod@users.noreply.github.com>
2018-11-13 19:54:54 -0600

    Add files via upload

--- Ian/inTankHardware.java
+++ Ian/inTankHardware.java
@@ -10,11 +10,11 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 //i might need 1 color senser and 1 servo
 
 public class inTankHardware {
-    DcMotor[] ITMotors = new DcMotor[3];
+    DcMotor[] ITMotors = new DcMotor[2];
     TouchSensor[] InTouchS = new TouchSensor[3];
     ColorSensor[] InTColor = new ColorSensor[1];
     Servo[] INTServo = new Servo[1];
-
+    DcMotor[] ITMotersTwo = new DcMotor[1];
     HardwareMap ITHwMap = null;
 
     public void init(HardwareMap ahwMap) {
@@ -26,23 +26,54 @@ public class inTankHardware {
             ITMotors[i].setDirection(DcMotor.Direction.FORWARD);
             ITMotors[i].setPower(0);
         }
-        for (int i = 0;i < InTouchS.length; i++){
-            InTouchS[i] = ITHwMap.get(TouchSensor.class,"InTTouch" +i);
+        for (int i = 0; i < InTouchS.length; i++) {
+            InTouchS[i] = ITHwMap.get(TouchSensor.class, "InTTouch" + i);
         }
         // If I need it!!!!!!
-//        for(int i = 0 ; i < InTColor.length; i++){
-//            InTColor[i] = ITHwMap.get(ColorSensor.class,"InTColor" + i);
-//
-//        }
-//        for(int i = 0 ; i < INTServo.length; i++){
-//            INTServo[i] = ITHwMap.get(Servo.class, "InTServo");
-//
-//        }
+        for (int i = 0; i < InTColor.length; i++) {
+            InTColor[i] = ITHwMap.get(ColorSensor.class, "InTColor" + i);
+
+        }
+        for (int i = 0; i < INTServo.length; i++) {
+            INTServo[i] = ITHwMap.get(Servo.class, "InTServo");
+
+        }
+        for(int i =0 ; i < ITMotersTwo.length; i++){
+            ITMotersTwo[i] = ITHwMap.get(DcMotor.class, "ITMotor2" + i);
+            ITMotersTwo[i].setDirection(DcMotor.Direction.FORWARD);
+            ITMotersTwo[i].setPower(0);
+            ITMotersTwo[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            ITMotersTwo[i].getCurrentPosition();
+        }
     }
-    public void  InTankHeader(a){
-        while(a){
-            ITMotors[0].setPower(.06);
+
+    public void inTankHeader(double power) {
+        ITMotors[0].setPower(power);
+    }
+
+    public void inTankArmLiftLower(double power,boolean x,boolean a) {
+//        // I want to lift and lower the arm
+        if(x){
+            ITMotors[1].setPower(power);
+            if (a){
+                ITMotors[1].setPower(-power);
+            }
+            if(InTouchS[0].isPressed()) {
+                ITMotors[1].setPower(0);
+            }
+            else if (InTouchS[1].isPressed()){
+                ITMotors[1].setPower(0);
+            }
         }
 
+    }
+public void inTankArmRiseLower (double power,boolean y, boolean b) {
+    //rase and lower arm
+    if (y) {
+        ITMotersTwo[0].setPower(power);
+        if (b) {
+            ITMotersTwo[0].setPower(-power);
+            }
+        }
     }
 }
--- Ian/inTankOpMode.java
+++ Ian/inTankOpMode.java
@@ -1,11 +1,70 @@
 package org.firstinspires.ftc.teamcode;
-
+import java.lang.Math;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class inTankOpMode {
 
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+public class inTankOpMode extends OpMode {
+    inTankHardware robot = new inTankHardware();
+    Servo[] INTServo = new Servo[1];
+    HardwareMap ITHwMap = null;
+
+
+    @Override
+    public void init(){
+        robot.init(hardwareMap);
+        for (int i = 0; i < INTServo.length; i++) {
+            INTServo[i] = ITHwMap.get(Servo.class, "InTServo");
+
+        }
+    }
+    @Override
+    //to start the the robot
+    public void start() {
+    }
+    @Override
+    public void loop(){
+        boolean a = gamepad1.dpad_left;
+        boolean b = gamepad1.dpad_right;
+
+        if (a){
+            INTServo[0].setPosition(1);
+        }
+        if (b){
+            INTServo[0].setPosition(-1);
+        }
+        else {
+            INTServo[0].setPosition(0);
+        }
+    }
+//    @Override
+//    // it adds the movement fuction to the start of the robot planes (driver control).
+//    public void loop () {
+//        double power = 0.06;
+//       boolean a = gamepad1.a;//used
+//        boolean bumper = gamepad1.right_bumper;//used
+//
+//        boolean x = gamepad1.x;//used
+//       boolean y = gamepad1.y;
+//       boolean b = gamepad1.b;
+//
+//        while(a && bumper){
+//            // intank header ativaction
+//            robot.inTankHeader(power);
+//        }
+//        while(x && bumper){
+//            robot.inTankHeader(-power);
+//        }
+//        while(x){
+//            // lift and lower
+//            robot.inTankArmLiftLower(power , x, a);
+//        }
+//        while(y){
+//            // rasied and lower
+//            robot.inTankArmRiseLower(power, y , b);
+//        }
+//    }
 }
-
-
-
