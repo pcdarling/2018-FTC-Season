c270ec1e0298352a4b92e4c1fa2b7d3905d9cff2
Brandon McGrew <42500154+xxiFoxTrot@users.noreply.github.com>
2019-01-27 11:50:17 -0600

    Sampling Code from reddit placed into function and thread

---
+++ test/ObjectDetection.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+import java.util.List;
+
+@Autonomous(name = "sampling", group = "testing")
+public class ObjectDetection extends OpMode {
+
+    public VuforiaHardware camera = new VuforiaHardware();
+
+    @Override
+    public void init(){
+        camera.init(hardwareMap);
+    }
+    @Override
+    public void start(){}
+
+    @Override
+    public void loop(){
+        /** Activate Tensor Flow Object Detection. */
+        if (camera.tfod != null) {
+            camera.tfod.activate();
+        }
+
+            if (camera.tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = camera.tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
+                    if (updatedRecognitions.size() == 3) {
+                        int goldMineralX = -1;
+                        int silverMineral1X = -1;
+                        int silverMineral2X = -1;
+                        for (Recognition recognition : updatedRecognitions) {
+                            if (recognition.getLabel().equals(camera.LABEL_GOLD_MINERAL)) {
+                                goldMineralX = (int) recognition.getLeft();
+                            } else if (silverMineral1X == -1) {
+                                silverMineral1X = (int) recognition.getLeft();
+                            } else {
+                                silverMineral2X = (int) recognition.getLeft();
+                            }
+                        }
+                        if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                            if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                                telemetry.addData("Gold Mineral Position", "Left");
+                            } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                                telemetry.addData("Gold Mineral Position", "Right");
+                            } else {
+                                telemetry.addData("Gold Mineral Position", "Center");
+                            }
+                        }
+                    }
+                    telemetry.update();
+                }
+            }
+    }
+    @Override
+    public void stop(){
+
+    }
+
+}
